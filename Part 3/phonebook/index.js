11ee9b8418586151814393c05402bd2a1e391eb2
charmainelow1 <125853983+charmainelow1@users.noreply.github.com>
2024-06-26 17:19:50 +0800

    3.11

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -9,6 +9,7 @@ var morgan = require('morgan')
 morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.use(express.static('dist'))
 
 let persons = [
     { 
